d476f33001fb1ea05a155d10ded73b4e8e25dfa9
vacharbhargav008 <vismay.bhargav@gmail.com>
2024-05-28 17:52:28 -0700

    Fix tests

--- src/test/java/tox/math/MatrixDTest.java
+++ src/test/java/tox/math/MatrixDTest.java
@@ -2,15 +2,15 @@ package tox.math;
 
 
 import org.junit.jupiter.api.Test;
-import tox.core.math.MatrixD;
+import tox.core.math.Matrix;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MatrixDTest {
     @Test
     void testEquals() {
-        MatrixD mat1 = new MatrixD(2, 2);
-        MatrixD mat2 = new MatrixD(2, 2);
-        assertEquals(mat1.equals(mat2), true);
+        Matrix mat1 = new Matrix(2, 2);
+        Matrix mat2 = new Matrix(2, 2);
+        assertEquals(mat1, mat2);
     }
 }
